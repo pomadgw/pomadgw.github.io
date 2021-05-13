4cae11f1cebe65fe8c5124d913f0e52c192fc384
Rahadian Yusuf <rahadian.y@gmail.com>
2021-05-13 22:30:03 +0700

    add init for blog plugin

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -80,6 +80,40 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      '@vuepress/plugin-blog',
+      {
+        directories: [
+          {
+            // Unique ID of current classification
+            id: 'post',
+            // Target directory
+            dirname: '_posts',
+            // Path of the `entry page` (or `list page`)
+            path: '/',
+            layout: 'Index',
+            itemLayout: 'Layout',
+            pagination: {
+              lengthPerPage: 2,
+            }
+          },
+        ],
+        frontmatters: [
+          {
+            // Unique ID of current classification
+            id: 'tag',
+            // Decide that the frontmatter keys will be grouped under this classification
+            keys: ['tag'],
+            // Path of the `entry page` (or `list page`)
+            path: '/tag/',
+            // Layout of the `entry page`
+            layout: 'Tags',
+            // Layout of the `scope page`
+            scopeLayout: 'Tag'
+          },
+        ],
+      }
+    ],
   ],
 
   permalink: '/:year/:month/:day/:slug.html',
