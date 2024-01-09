d54b16242b2122aed042aef8befb60e167c5d0e8
Rahadian Yusuf <rahadian.y@gmail.com>
2024-01-09 19:49:10 +0700

    fix linter for ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,8 @@ module.exports = {
   },
   "extends": [
     "standard-with-typescript",
-    "plugin:vue/vue3-essential"
+    "plugin:vue/vue3-essential",
+    "plugin:astro/recommended"
   ],
   "overrides": [
     {
@@ -21,9 +22,9 @@ module.exports = {
       }
     },
     {
-      extends: ["plugin:astro/recommended"],
       files: ['*.astro'],
       parser: 'astro-eslint-parser',
+      processor: "astro/client-side-ts",
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro']
@@ -32,11 +33,27 @@ module.exports = {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
       }
-    }
+    },
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+    // ...
   ],
   "parserOptions": {
     "ecmaVersion": "latest",
-    "sourceType": "module"
+    "sourceType": "module",
   },
   "plugins": [
     "vue"
