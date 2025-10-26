c22e2acfabce30039049d280a77312e51e3de45a
Rahadian Yusuf <rahadian.y@gmail.com>
2025-10-26 22:11:00 +0700

    Update 20251026 (#51)
    * feat: update
    
    * feat: upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 
 import vue from '@astrojs/vue'
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://the.pomadgw.xyz',
-  integrations: [mdx(), sitemap(), tailwind(), vue({ appEntrypoint: '/src/pages/_app' })]
+  integrations: [mdx(), sitemap(), vue({ appEntrypoint: '/src/pages/_app' })],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
